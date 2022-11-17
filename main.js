6f8b9cabd3c02c0dd5d49623304e3ddf2ef4114e
Vilte N <viltenor@gmail.com>
2022-11-17 15:00:59 +0000

    created the structure

---
+++ main.js
@@ -0,0 +1,24 @@
+const indNeedHelpBtn = document.getElementById('individual-need-help');
+
+indNeedHelpBtn.addEventListener('click',function(event){
+    window.location='ind-need-help.html';
+})
+
+
+const indCanHelpBtn = document.getElementById('individual-can-help');
+
+indCanHelpBtn.addEventListener('click',function(event){
+    window.location='ind-can-help.html';
+})
+
+const busCanHelpBtn = document.getElementById('business-can-help');
+
+busCanHelpBtn.addEventListener('click',function(event){
+    window.location='bus-can-help.html';
+})
+
+const busNeedHelpBtn = document.getElementById('business-need-help');
+
+busNeedHelpBtn.addEventListener('click',function(event){
+    window.location='bus-need-help.html';
+})
